291916853cff8775253dd2198d60c42777e022e8
fitbouncer <bhuinjohn@gmail.com>
2023-02-21 22:37:53 +0530

    vite is added telwind is added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
+import store from './store'
+import './index.css';
+
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+import App from "./App.vue"
 import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
-
+app.use(store)
 app.use(createPinia())
 app.use(router)
 
---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        test:'Pradipta S Bhuin'
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+})
+
+export default store
